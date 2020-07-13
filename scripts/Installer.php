d6a550df758549c0ef61a9395a8121f00b9e6de9
Duncan Sutter <dsutter@qualivera.com>
2020-07-13 15:09:12 -0400

    Installer

--- scripts/Installer.php
+++ scripts/Installer.php
@@ -11,6 +11,7 @@ class Installer
     {
         $composer = $event->getComposer();
 
+        echo "Gonna install\n";
         system("ln -s /opt/app-root/src/vendor/dsutter-gc/remote-manage/vtest.php /opt/app-root/src/html/vtest.php");
     }
 }
