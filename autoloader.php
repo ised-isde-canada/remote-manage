43482928a71f6da7495b6ca735604af868bfbece
Duncan Sutter <dsutter@qualivera.com>
2020-07-17 17:23:03 -0400

    Best autoloader!!

--- autoloader.php
+++ autoloader.php
@@ -1,16 +1,6 @@
 <?php
 
-// Set up autoloader, which assumes that we are running in a vendor folder.
-// If this proves problematic, then see vendor/drush/drush/drush.php for ideas to make this better
+// Finally found this solution here https://stackoverflow.com/questions/12818690/using-composers-autoload
 
-spl_autoload_register(function ($class) {
-    if (strpos($class, 'RemoteManage\\') !== 0) {
-        return;
-    }
-
-    $file = __DIR__.'/src'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, strlen('RemoteManage'))).'.php';
-
-    if (is_file($file)) {
-        require_once $file;
-    }
-});
+$loader = require 'vendor/autoload.php';
+$loader->addPsr4('RemoteManage\\', __DIR__.'/src/');
